01238a22fce9ad8b107900cf47b0740ac0a9f297
Jiseung <js43o@naver.com>
2025-03-27 11:09:38 +0900

    ✨ 입력 데이터 형상에 맞도록 STN 블록 인자 수정

--- models/coarse_restoration/stn.py
+++ models/coarse_restoration/stn.py
@@ -6,16 +6,13 @@ from torch.nn import functional as F
 
 
 class STNBlock(nn.Module):
-    def __init__(self):
-        super(STN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+    def __init__(self, in_ch, in_res):
+        super().__init__()
+
+        self.fc_res = (in_res - 14) / 4  # localization 연산 후 크기 계산
 
         self.localization = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=7),
+            nn.Conv2d(in_ch, 8, kernel_size=7),
             nn.MaxPool2d(2, stride=2),
             nn.ReLU(True),
             nn.Conv2d(8, 10, kernel_size=5),
@@ -24,7 +21,9 @@ class STNBlock(nn.Module):
         )
 
         self.fc_loc = nn.Sequential(
-            nn.Linear(10 * 3 * 3, 32), nn.ReLU(True), nn.Linear(32, 3 * 2)
+            nn.Linear(10 * self.fc_res * self.fc_res, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2),
         )
 
         self.fc_loc[2].weight.data.zero_()
@@ -34,7 +33,7 @@ class STNBlock(nn.Module):
 
     def forward(self, x):
         xs = self.localization(x)
-        xs = xs.view(-1, 10 * 3 * 3)
+        xs = xs.view(-1, 10 * self.fc_res * self.fc_res)
         theta = self.fc_loc(xs)
         theta = theta.view(-1, 2, 3)
 
