931023f6fbad7ba78cf42bf39d78ef3506cefd58
Jiseung <js43og@gmail.com>
2025-04-16 10:41:44 +0900

    ✨ NAF 블록 채널 수 확장

--- models/cr/model.py
+++ models/cr/model.py
@@ -5,21 +5,24 @@ from .stn import STNBlock
 
 
 class NAF_STN_Block(nn.Module):
-    def __init__(self, in_ch: int, in_res: int, num_naf: int, sampling=None):
+    def __init__(self, in_channel: int, in_resolution: int, num_naf: int, sampling=None):
         super().__init__()
 
-        self.nfbs = nn.Sequential(*[NAFBlock(in_ch) for _ in range(num_naf)])
-        self.stn = STNBlock(in_ch, in_res)
+        self.nfbs = nn.Sequential(*[NAFBlock(in_channel) for _ in range(num_naf)])
+        self.stn = STNBlock(in_channel, in_resolution)
         if sampling == "down":
-            self.sampling = nn.AvgPool2d(2)
+            self.sampling = nn.Conv2d(in_channel, in_channel*2, 2, 2)
         elif sampling == "up":
-            self.sampling = nn.Upsample(scale_factor=2.0)
+            self.sampling = nn.Sequential(
+                    nn.Conv2d(in_channel,in_channel*2, 1, bias=False),
+                    nn.PixelShuffle(2)
+                )
         else:
             self.sampling = nn.Identity()
 
     def forward(self, x: torch.Tensor):
         x = self.nfbs(x)
-        x = self.stn(x)
+        # x = self.stn(x)
         x = self.sampling(x)
 
         return x
@@ -28,31 +31,41 @@ class NAF_STN_Block(nn.Module):
 class CoarseRestoration(nn.Module):
     def __init__(self):
         super().__init__()
+        
+        width = 32
+        
+        self.intro = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+        self.outro = nn.Conv2d(in_channels=width, out_channels=3, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
 
         self.encoders = nn.Sequential(
-            NAF_STN_Block(3, 128, num_naf=2, sampling="down"),
-            NAF_STN_Block(3, 64, num_naf=2, sampling="down"),
-            NAF_STN_Block(3, 32, num_naf=4, sampling="down"),
-            NAF_STN_Block(3, 16, num_naf=8, sampling="down"),
+            NAF_STN_Block(width, 128, num_naf=2, sampling="down"),
+            NAF_STN_Block(width*2, 64, num_naf=2, sampling="down"),
+            NAF_STN_Block(width*4, 32, num_naf=4, sampling="down"),
+            NAF_STN_Block(width*8, 16, num_naf=8, sampling="down"),
         )
-        self.middle_blocks = NAF_STN_Block(3, 8, num_naf=8)
+        self.middle_blocks = NAF_STN_Block(width*16, 8, num_naf=8)
         self.decoders = nn.Sequential(
-            NAF_STN_Block(3, 8, num_naf=2, sampling="up"),
-            NAF_STN_Block(3, 16, num_naf=2, sampling="up"),
-            NAF_STN_Block(3, 32, num_naf=2, sampling="up"),
-            NAF_STN_Block(3, 64, num_naf=2, sampling="up"),
+            NAF_STN_Block(width*16, 8, num_naf=2, sampling="up"),
+            NAF_STN_Block(width*8, 16, num_naf=2, sampling="up"),
+            NAF_STN_Block(width*4, 32, num_naf=2, sampling="up"),
+            NAF_STN_Block(width*2, 64, num_naf=2, sampling="up"),
         )
 
     def forward(self, x: torch.Tensor):
         enc_skips = []
+        
+        x = self.intro(x)
         for encoder in self.encoders:
             x = encoder(x)
             enc_skips.append(x)
 
         x = self.middle_blocks(x)
-
         for decoder, enc_skip in zip(self.decoders, enc_skips[::-1]):
             x = x + enc_skip
             x = decoder(x)
+
+        x = self.outro(x)
 
         return x
