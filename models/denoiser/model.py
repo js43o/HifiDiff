61265e330598754041dfa70a8af972f4735529ff
Jiseung <js43og@gmail.com>
2025-04-18 15:58:13 +0900

    ✨ Denoiser 모델을 조건부 NAFNet으로 변경

--- models/denoiser/model.py
+++ models/denoiser/model.py
@@ -1,6 +1,157 @@
+import math
 import torch
 from torch import nn
-from models.common.naf import NAFBlock
+from einops import rearrange, reduce
+
+from models.common.naf_utils import LayerNorm2d
+
+
+class SimpleGate(nn.Module):
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1 * x2
+
+
+# sinusoidal positional embeds
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        device = x.device
+        half_dim = self.dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
+        emb = x[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
+
+
+class NAFBlock(nn.Module):
+    def __init__(
+        self, c, time_emb_dim=None, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.0
+    ):
+        super().__init__()
+        self.mlp = (
+            nn.Sequential(SimpleGate(), nn.Linear(time_emb_dim // 2, c * 4))
+            if time_emb_dim
+            else None
+        )
+
+        dw_channel = c * DW_Expand
+        self.conv1 = nn.Conv2d(
+            in_channels=c,
+            out_channels=dw_channel,
+            kernel_size=1,
+            padding=0,
+            stride=1,
+            groups=1,
+            bias=True,
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=dw_channel,
+            out_channels=dw_channel,
+            kernel_size=3,
+            padding=1,
+            stride=1,
+            groups=dw_channel,
+            bias=True,
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=dw_channel // 2,
+            out_channels=c,
+            kernel_size=1,
+            padding=0,
+            stride=1,
+            groups=1,
+            bias=True,
+        )
+
+        # Simplified Channel Attention
+        self.sca = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(
+                in_channels=dw_channel // 2,
+                out_channels=dw_channel // 2,
+                kernel_size=1,
+                padding=0,
+                stride=1,
+                groups=1,
+                bias=True,
+            ),
+        )
+
+        # SimpleGate
+        self.sg = SimpleGate()
+
+        ffn_channel = FFN_Expand * c
+        self.conv4 = nn.Conv2d(
+            in_channels=c,
+            out_channels=ffn_channel,
+            kernel_size=1,
+            padding=0,
+            stride=1,
+            groups=1,
+            bias=True,
+        )
+        self.conv5 = nn.Conv2d(
+            in_channels=ffn_channel // 2,
+            out_channels=c,
+            kernel_size=1,
+            padding=0,
+            stride=1,
+            groups=1,
+            bias=True,
+        )
+
+        self.norm1 = LayerNorm2d(c)
+        self.norm2 = LayerNorm2d(c)
+
+        self.dropout1 = (
+            nn.Dropout(drop_out_rate) if drop_out_rate > 0.0 else nn.Identity()
+        )
+        self.dropout2 = (
+            nn.Dropout(drop_out_rate) if drop_out_rate > 0.0 else nn.Identity()
+        )
+
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+
+    def time_forward(self, time, mlp):
+        time_emb = mlp(time)
+        time_emb = rearrange(time_emb, "b c -> b c 1 1")
+        return time_emb.chunk(4, dim=1)
+
+    def forward(self, x):
+        inp, time = x
+        shift_att, scale_att, shift_ffn, scale_ffn = self.time_forward(time, self.mlp)
+
+        x = inp
+
+        x = self.norm1(x)
+        x = x * (scale_att + 1) + shift_att
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.sg(x)
+        x = x * self.sca(x)
+        x = self.conv3(x)
+
+        x = self.dropout1(x)
+
+        y = inp + x * self.beta
+
+        x = self.norm2(y)
+        x = x * (scale_ffn + 1) + shift_ffn
+        x = self.conv4(x)
+        x = self.sg(x)
+        x = self.conv5(x)
+
+        x = self.dropout2(x)
+
+        x = y + x * self.gamma
+
+        return x, time
 
 
 class Denoiser(nn.Module):
@@ -9,6 +160,19 @@ class Denoiser(nn.Module):
 
         img_channel = 3
         width = 32
+
+        self.img_channel = img_channel
+        self.upscale = 1
+        fourier_dim = width
+        sinu_pos_emb = SinusoidalPosEmb(fourier_dim)
+        time_dim = width * 4
+
+        self.time_mlp = nn.Sequential(
+            sinu_pos_emb,
+            nn.Linear(fourier_dim, time_dim * 2),
+            SimpleGate(),
+            nn.Linear(time_dim, time_dim),
+        )
 
         self.intro = nn.Conv2d(
             in_channels=img_channel,
@@ -37,11 +201,13 @@ class Denoiser(nn.Module):
 
         chan = width
         for num in [2, 2, 4, 8]:
-            self.encoders.append(nn.Sequential(*[NAFBlock(chan) for _ in range(num)]))
+            self.encoders.append(
+                nn.Sequential(*[NAFBlock(chan, time_dim) for _ in range(num)])
+            )
             self.downs.append(nn.Conv2d(chan, 2 * chan, 2, 2))
             chan = chan * 2
 
-        self.middle_blks = nn.Sequential(*[NAFBlock(chan) for _ in range(8)])
+        self.middle_blks = nn.Sequential(*[NAFBlock(chan, time_dim) for _ in range(8)])
 
         for num in [2, 2, 2, 2]:
             self.ups.append(
@@ -50,30 +216,35 @@ class Denoiser(nn.Module):
                 )
             )
             chan = chan // 2
-            self.decoders.append(nn.Sequential(*[NAFBlock(chan) for _ in range(num)]))
+            self.decoders.append(
+                nn.Sequential(*[NAFBlock(chan, time_dim) for _ in range(num)])
+            )
 
-    def forward(self, input: torch.Tensor):
-        B, C, H, W = input.shape
+    def forward(self, inp, time, cond=None):
+        if isinstance(time, int) or isinstance(time, float):
+            time = torch.tensor([time]).to(inp.device)
 
-        x = self.intro(input)
+        x = inp
+        if cond:
+            x = torch.cat([x, cond], dim=1)
+        B, C, H, W = x.shape
+
+        t = self.time_mlp(time)
+        x = self.intro(x)
 
         enc_skips = []
-
         for encoder, down in zip(self.encoders, self.downs):
-            x = encoder(x)
+            x, _ = encoder([x, t])
             enc_skips.append(x)
             x = down(x)
 
-        for middle_blk in self.middle_blks:
-            x = middle_blk(x)
-            # TODO: element-wise addition with output of IDC
-
+        x, _ = self.middle_blks([x, t])
         for decoder, up, enc_skip in zip(self.decoders, self.ups, enc_skips[::-1]):
             x = up(x)
             x = x + enc_skip
-            x = decoder(x)
+            x, _ = decoder([x, t])
 
         x = self.ending(x)
-        x = x + input
+        x = x[..., :H, :W]
 
-        return x[:, :, :H, :W]
+        return x
